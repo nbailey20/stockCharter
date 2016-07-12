8c017d2dd3ed0eb28d5e42ce650b18aa3a2ffdb2
bartowski20 <nbailey@g.hmc.edu>
2016-07-12 23:41:31 +0000

    bare-bones app to listen on port

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (app) {
+  
+  app.route("/")
+    .get(function (req, res) {
+       res.send("Hello world"); 
+    });
+    
+};
---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require("express");
+var routes = require("./app/routes/index.js");
+require("node-env-file")(".env");
+var app = express();
+
+app.use("/public", express.static(process.cwd() + "/public"));
+routes(app);
+
+app.listen(process.env.PORT, function() {
+    console.log("App listening on port " + process.env.PORT + "...");
+})
